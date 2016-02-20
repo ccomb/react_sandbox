d8343a8b853f77fcbaa90d37666b897be172d3fe
Christophe Combelles <ccomb@free.fr>
2016-02-20 20:10:24 +0200

    loop over dummy apps

--- js/app.jsx
+++ js/app.jsx
@@ -113,41 +113,24 @@ const RootComponent = connect(state=>({state}))(React.createClass({
         const childroute = {segments, current:current+1};
         switch(segments[current+1]) { // TODO make it pluggable
             case undefined:
-                return (<div className='row center-xs around center-xs start-sm' style={{margin: 0, padding: '1em'}}>
-                    <div className='col-xs-12 col-sm-6 col-md-3' style={{marginBottom: '1em'}}>
-                        <a href='#/bo/' className="box">
-                            <Card>
-                                <CardMedia overlay={
-                                    <CardTitle title="Back Office" subtitle="Stunning offline-first and mobile-first back office" />
-                                }>
-                                <img src="http://lorempixel.com/g/320/198/abstract/1" />
-                                </CardMedia>
-                            </Card>
-                        </a>
-                    </div>
-                    <div className='col-xs-12 col-sm-6 col-md-3'>
-                        <a href='#/pos/' className="box">
-                            <Card>
-                                <CardMedia overlay={
-                                    <CardTitle title="Point of Sale" subtitle="Stunning offline-first and mobile-first point of sale" />
-                                }>
-                                <img src="http://lorempixel.com/g/320/198/abstract/2" />
-                                </CardMedia>
-                            </Card>
-                        </a>
-                    </div>
-                    <div className='col-xs-12 col-sm-6 col-md-3'>
-                        <a href='#/bi/' className="box">
-                            <Card>
-                                <CardMedia overlay={
-                                    <CardTitle title="Business Intelligence" subtitle="Stunning offline-first and mobile-first business intelligence" />
-                                }>
-                                <img src="http://lorempixel.com/g/320/198/abstract/3" />
-                                </CardMedia>
-                            </Card>
-                        </a>
-                    </div>
-                </div>);
+                return (
+                    <div className='row center-xs around center-xs start-sm' style={{margin: 0, padding: '1em'}}>
+                        {[{url: 'bo', title: 'Back Office', desc: 'Stunning offline-first and mobile-first back office'},
+                          {url: 'pos', title: 'Point of Sale', desc: 'Stunning offline-first and mobile-first point of sale'},
+                          {url: 'bi', title: 'Business Intelligence', desc: 'Stunning offline-first and mobile-first business intelligence'},
+                         ].map((app)=>
+                        <div className='col-xs-12 col-sm-6 col-md-3' style={{marginBottom: '1em'}}>
+                            <a href={`#/${app.url}/`} className="box">
+                                <Card>
+                                    <CardMedia overlay={
+                                        <CardTitle title={`${app.title}`} subtitle={`${app.desc}`} />
+                                    }>
+                                    <div style={{background: 'linear-gradient(45deg, #d8e0de 0%,#aebfbc 22%,#99afab 33%,#8ea6a2 50%,#829d98 60%,#4e5c5a 82%,#0e0e0e 100%)', height: '14.5em'}}/>
+                                    </CardMedia>
+                                </Card>
+                            </a>
+                        </div>)}
+                    </div>);
             case 'bo':
                 return <BackOffice route={childroute}/>;
             default:
