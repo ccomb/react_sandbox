18fbec1b2d3e2b836a7d1bff00477d571bfb3f60
Christophe Combelles <ccomb@free.fr>
2016-02-27 16:20:40 +0200

    Removed webpack conf to use material-ui/src instead of material-ui/lib

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,10 @@
 var webpack = require("webpack");
-//var path = require("path");
 
 module.exports = {
   entry: [
     "webpack-dev-server/client?http://0.0.0.0:3000",
     "webpack/hot/only-dev-server",
     "./js/app.jsx",
-//    "./material-ui/src/index.js",
     ],
   output: {
     filename: "bundle.js",
@@ -14,10 +12,6 @@ module.exports = {
   },
   resolve: {
     extensions: ["", ".js", ".jsx"],
-//    alias: {
-//      'material-ui/lib': path.resolve(__dirname, 'material-ui/src'),
-//      'material-ui': path.resolve(__dirname, 'material-ui/src'),
-//    }
   },
   module: {
         loaders: [
